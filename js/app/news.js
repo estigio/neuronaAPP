a0c27527d343bf3bcef83d2cb8027a4402c24b4e
walther smith franco otero <wfrancootero@gmail.com>
2014-10-13 09:09:13 -0500

    dev news

--- js/app/news.js
+++ js/app/news.js
@@ -1,1 +1,61 @@
-news.js
+/*
+ * Created by
+ * User: Walher smith franco otero
+ * Date: 2014-10-13
+ * Contact: wfrancootero@gmail.coom | about.me/walthersmith
+ */
+
+var API_SOURCE ="http://192.168.1.4:81/ApiNeurona/api/";
+var SourceImagen = "http://192.168.1.4:81/ApiNeurona/files/"
+
+$.support.cors = true;
+$.mobile.allowCrossDomainPages = true;
+function listNews() {
+	$.mobile.loading( "show");
+	var content = "";	
+	$.ajax({
+		url: API_SOURCE+"newsList/",
+		type:'GET',
+		async: true,
+		format:"jsonp",
+		crossDomain: true,
+		error: function(jqXHR, textStatus, errorThrown){
+                       console.log("hi");
+                       console.log(jqXHR);
+                       console.log(textStatus);
+                       console.log(errorThrown);
+                        //do stuff
+                },
+        cache:false
+
+	}).done( function (data){
+		
+		$.each( data, function ( i, item ){
+			content  += '<li                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                class="nw"><a href="#id'+item['id']+'" >'+
+                            "<img src=\""+SourceImagen+"news/"+item['img']+"\">"+
+                        "<h2>"+item['title']+"</h2>"+
+                        "<p>"+item["description"].substring(0, 30)+"</p></a>"+
+                        "</li>";
+		});
+		$("#listnews").append(content);
+		$("#listnews").listview( "refresh" );
+
+		$.mobile.loading( "hide");
+	});
+}
+
+function cargarNoticia (id) {
+	$.ajax({
+		url: API_SOURCE+"news?id="+id,
+		async: true,
+		format:"jsonp"
+	}).done(function (data){
+		$.each( data, function ( i, item ){
+				$("#title").html(item['titulo']);
+				$("#image").attr("src",SourceImagen+item['pic']);
+				$("#description").html(item.Descripcion);
+		});
+
+	});
+}
+
